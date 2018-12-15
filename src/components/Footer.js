18c11f3450b765a149e1b65010e81b62e9cbadec
slm15 <samuel.mnet@gmail.com>
2018-12-15 17:36:24 +0100

    modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
-        <span>Copyright © 2018 Designed by <a href="https://bytes.io" target='_blank' title="Bytes">Bytes.io</a>. All rights reserved.</span>
+        <span>Copyright © 2018 Designed by <a href="https://bytes.io" target='_blank' title="Bytes">Bytes.io</a>. An <a href="https://iota.org" target='_blank' title="Bytes">IOTA</a> powered project.</span>
       </footer>
     )
   }
